0242c777ad1e163708f58e85f9ddf5adcf8cea53
Victor Brambilla <victor-brambilla@hotmail.com>
2022-01-30 18:30:38 -0400

    set loading

--- contacts/src/components/RegisterContact.js
+++ contacts/src/components/RegisterContact.js
@@ -6,6 +6,7 @@ import { ArrowBack } from '@mui/icons-material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { dataService } from '../services/dataService';
 import { GlobalContext } from '../contexts/GlobalStorage';
+import SaveIcon from '@mui/icons-material/Save';
 
 const CssTextField = styled(TextField)({
  margin:'10px',
@@ -15,6 +16,7 @@ const CssTextField = styled(TextField)({
 
 export default function RegisterContact(props) {
     const global = React.useContext(GlobalContext);
+    const [loading, setLoading]=useState(false)
 
     const [email, setEmail] = useState('');
     const [firstName, setFirstName] = useState(''); 
@@ -31,6 +33,7 @@ export default function RegisterContact(props) {
 
     const handleSubmit =  (event) => {
         event.preventDefault();
+        setLoading(true);
         const data ={
           Email: email,
           First_Name: firstName,
@@ -44,31 +47,34 @@ export default function RegisterContact(props) {
           Mailing_Country: mailingCountry
         }
 
-       
          dataService.createContact(data).then(()=>{
-          global.getCont()
-          alert("Sended")
+          global.getCont();
+          alert("Sended!");
           return props.closeModal();
          })
-        
+         .finally(()=>setLoading(false));
       };
 
-    const getContact = React.useCallback( async () => {
-      const data = await dataService.getContact(props.id);
-      setEmail(data[0].Email);
-      setFirstName(data[0].First_Name);
-      setLastName(data[0].Last_Name);
-      setPhone(data[0].Phone);
-      setDepartment(data[0].Department);
-      setMailingStreet(data[0].Mailing_Street);
-      setMailingCity(data[0].Mailing_City);
-      setMailingState(data[0].Mailing_State);
-      setMailingZip(data[0].Mailing_Zip);
-      setMailingCountry(data[0].Mailing_Country);
+    const getContact = React.useCallback(() => {
+      setLoading(true);
+      dataService.getContact(props.id).then(data=>{
+        setEmail(data[0].Email);
+        setFirstName(data[0].First_Name);
+        setLastName(data[0].Last_Name);
+        setPhone(data[0].Phone);
+        setDepartment(data[0].Department);
+        setMailingStreet(data[0].Mailing_Street);
+        setMailingCity(data[0].Mailing_City);
+        setMailingState(data[0].Mailing_State);
+        setMailingZip(data[0].Mailing_Zip);
+        setMailingCountry(data[0].Mailing_Country);
+      })
+      .finally(()=>setLoading(false));
     },[props.id])
     
     const updateContact =  (event) => {
       event.preventDefault();
+      setLoading(true);
         const data ={
           Email: email,
           First_Name: firstName,
@@ -84,14 +90,15 @@ export default function RegisterContact(props) {
 
         dataService.updateContact(props.id, data).then(()=>{
           global.getCont();
-          alert("Updated")
+          alert("Updated!");
           return props.closeModal();
         })
+        .finally(()=>setLoading(false));
     }
 
     React.useEffect(()=>{
       if(props.id){
-        return getContact()
+        return getContact();
       }
     },[getContact, props.id])  
 
@@ -204,8 +211,10 @@ export default function RegisterContact(props) {
           </Box>
               
           <Box sx={{display:'flex', justifyContent:'center',mt:'20px'}}>
-              {props.id ? <Button color='primary'  onClick={updateContact} variant="contained" sx={{m:'0 10px'}} startIcon={<AddIcon />}>Edit</Button> : 
-              <Button color='primary' type='submit' variant="contained" sx={{m:'0 10px'}} startIcon={<AddIcon />}>Send</Button>}
+              {props.id ? 
+              <LoadingButton color='primary' loading={loading} onClick={updateContact} variant="contained" sx={{m:'0 10px'}} startIcon={<AddIcon />}>Edit</LoadingButton> : 
+              <LoadingButton color='primary' loading={loading} startIcon={<SaveIcon />}  type='submit' variant="contained" sx={{m:'0 10px'}} startIcon={<AddIcon />}>Send</LoadingButton>
+              }
 
               <Button onClick={props.closeModal} color='error' variant="contained" sx={{m:'0 10px'}} startIcon={<ArrowBack />}>Cancel</Button>
           </Box>
